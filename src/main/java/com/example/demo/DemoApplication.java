f623f999862dfb17d097a370e2ffe196a68e890c
Daniel Pedersen <daniel@Daniels-MacBook-Pro.local>
2018-04-29 18:19:36 +0200

    something new

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,8 +3,6 @@ package com.example.demo;
 		import javafx.application.Application;
 		import org.springframework.boot.SpringApplication;
 		import org.springframework.boot.autoconfigure.SpringBootApplication;
-		import org.springframework.boot.builder.SpringApplicationBuilder;
-		import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class DemoApplication {
